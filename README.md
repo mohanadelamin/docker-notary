98dc22951ac416d00422526a84490abd58a4aeaf
Mohanad Elamin <mohanad.elamin@os3.nl>
2021-01-21 10:22:17 +0100

    Modify readme and some minor changes

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # docker-notary
+
+Script to install notary along with single registry in an Ubuntu VM.
+
+## Usage
+1. download the script
+```
+wget https://raw.githubusercontent.com/mohanadelamin/docker-notary/main/single_node_notary.sh
+```
+
+2. Add execute permission
+```
+chmod +x single_node_notary.sh
+```
+
+3. Run the script
+```
+./single_node_notary.sh
+```
--- single_node_notary.sh
+++ single_node_notary.sh
@@ -4,20 +4,38 @@ NOTARY_DIR="/root/notary"
 
 echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
 
+# Update packages
 sudo apt-get update -y
+
+# Remove any old docker packages
 sudo apt-get remove docker docker-engine docker.io containerd runc -y
+
+# Install required packages
 sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common vim jq wget -y
+
+# Add docker GPG key
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Add docker repo
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Install docker
 sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+# Install docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# Clone Notary repo
 sudo git clone https://github.com/theupdateframework/notary.git $NOTARY_DIR
 
+# Build and install Notary
 sudo docker-compose $NOTARY_DIR/docker-compose.yml build -f
 sudo docker-compose $NOTARY_DIR/docker-compose.yml up -d
+sudo mkdir -p ~/.notary && cp $NOTARY_DIR/cmd/notary/config.json $NOTARY_DIR/cmd/notary/root-ca.crt ~/.notary
 
+# Install Notary client
 sudo curl -sSfLo /usr/local/bin/notary https://github.com/theupdateframework/notary/releases/download/v0.6.1/notary-Linux-amd64
 sudo chmod +x /usr/local/bin/notary
 
@@ -31,6 +49,7 @@ sudo cat <<\EOT > ~/.notary/config.json
 }
 EOT
 
+# run local registery
 sudo docker run -d -p 5000:5000 --restart always --name registry registry:2
 sudo mkdir -p ~/.docker/tls/127.0.0.1:4443/
 sudo cp $NOTARY_DIR/fixtures/notary-server.crt ~/.docker/tls/127.0.0.1\:4443/ca.crt
